b224f32391b6b91474afe8ce95d02652baaa7342
ComeOnTaBlazes <g00387818@gmit.ie>
2020-02-05 00:18:38 +0000

    wk3thefox

---
+++ thefox.py
@@ -0,0 +1,9 @@
+# Wk 3 assignment
+# Asks a user to input a string and outputs every second letter in reverse order
+
+a= input("Enter text:")
+
+#a ="The quick brown fox jumps over the lazy dog."
+
+
+print(a[::-2])
