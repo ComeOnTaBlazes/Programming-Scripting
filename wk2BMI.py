8743a134c6389d662710bcbcda9c4dddbd77e2a0
ComeOnTaBlazes <g00387818@gmit.ie>
2020-04-03 19:49:48 +0100

    Changed BMI.py to working file

--- wk2BMI.py
+++ wk2BMI.py
@@ -1,17 +1,12 @@
 #Calculation of BMI in m2 with user inputs
 
-def BMI(bmi):
-    bmi = Weight / HeightMsq
-    return(round(bmi,2))
-
-
 Weight = float(input("Enter weight in kg: "))
 Height = float(input("Enter height in cm: "))
 
 HeightMsq = (Height/100)**2
 
-#bmi = Weight / HeightMsq
+bmi = Weight / HeightMsq
 
 BMI = round(bmi,2)
 
-print("Your BMI is {} m2".format(BMI))
+print("Your BMI is {} m2".format(bmi))
